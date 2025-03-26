1ba0666a13e69f303f9412ceef573adf580c3a08
R1040604 <r1040604@student.thomasmore.be>
2025-03-26 12:41:19 +0100

    link sketch

--- sketch.js
+++ sketch.js
@@ -1,19 +1,2 @@
-class waterTracker{
-    constructor() {
-        this.totalWater = 0;
-        this.water = [];
-    }
-    
-    addWater(liter){
-        this.totalWater += amount;
-        console.log(`Je hebt tot nu toe ${liter}l water gedronken. Totaal: ${this.totalWater}liter`);
-    }
-    
-    getTotalWater(){
-        return this.totalWater;
-    }
-    
-}
-//const h3 = document.querySelector(".progres_container");
-
+import WaterTracker from "./Tracker/Tracker.js";
 
