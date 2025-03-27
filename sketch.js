d0d83875eecc47af688381d7665524966fb97808
R1040604 <r1040604@student.thomasmore.be>
2025-03-27 13:59:08 +0100

    Local storage

--- sketch.js
+++ sketch.js
@@ -1,2 +1,7 @@
 import WaterTracker from "./Tracker/Tracker.js";
 
+document.getElementById('opslaan').addEventListener('click', (e) => {
+    tracker.addWater(0.8); // Example: adding 0.8 liters
+    localStorage.setItem('totalWater', JSON.stringify(tracker.getTotalWater()));
+    console.log('Water intake saved to local storage.');
+});
