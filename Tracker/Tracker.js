970c6e108a9b2199238922f071dccbd3cbd051fe
R1040604 <r1040604@student.thomasmore.be>
2025-03-29 20:05:46 +0100

    haakje verplaatsing

--- Tracker/Tracker.js
+++ Tracker/Tracker.js
@@ -2,16 +2,17 @@ export default class WaterTracker{
     constructor() {
         this.totalWater = 0;
         this.water = [];
-} 
+    }
+
     addWater(liter){
         this.totalWater += liter;
         this.water.push(liter);
         console.log(`Je hebt tot nu toe ${liter}l water gedronken. Totaal: ${this.totalWater}liter`);
     }
-    
+
     getTotalWater(){
         return this.totalWater;
-    }
+    } 
 }
 
 //const h3 = document.querySelector(".progres_container");
